5fd941802db569752adc43f2d184805c6e4da564
Pranay Bansal <pranaybansal26@gmail.com>
2026-02-12 23:49:56 +0530

    turn wise betting to a round's pot

--- banker.cpp
+++ banker.cpp
@@ -8,10 +8,13 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <string.h>
+#include <algorithm>
 
 struct Player {
     int socket;
     int wallet;
+    int round_contribution; // How much they've put in THIS round
+    bool folded;
 };
 
 std::mutex game_mutex;
@@ -20,19 +23,31 @@ std::vector<std::string> turn_order;
 int turn_index = 0;
 int total_pot = 0;
 int starting_budget = 500;
+int last_bet = 0; // The current high bet to match
 
 void broadcast_game_state() {
-    // This function now handles its own locking
     std::lock_guard<std::mutex> lock(game_mutex);
     if (turn_order.empty()) return;
 
     std::string current_turn = turn_order[turn_index];
     for (auto const& [name, player] : players) {
-        std::string msg = "POT:" + std::to_string(total_pot) + 
-                          "|WALLET:" + std::to_string(player.wallet) + 
-                          "|TURN:" + current_turn + "\0";
-        send(player.socket, msg.c_str(), msg.length(), 0);
+        // P:Pot | W:Wallet | T:Turn | L:HighBet | C:YourContribution
+        std::string msg = "P:" + std::to_string(total_pot) + 
+                          "|W:" + std::to_string(player.wallet) + 
+                          "|T:" + current_turn + 
+                          "|L:" + std::to_string(last_bet) + 
+                          "|C:" + std::to_string(player.round_contribution) + "\0";
+        send(player.socket, msg.c_str(), msg.length() + 1, 0);
     }
+}
+
+void next_turn() {
+    if (turn_order.size() < 2) return;
+    int initial_index = turn_index;
+    do {
+        turn_index = (turn_index + 1) % turn_order.size();
+        if (turn_index == initial_index) break;
+    } while (players[turn_order[turn_index]].folded);
 }
 
 void handle_player(int client_socket) {
@@ -40,68 +55,74 @@ void handle_player(int client_socket) {
     memset(buffer, 0, 1024);
     int valread = recv(client_socket, buffer, 1024, 0);
     if (valread <= 0) return;
+    
     std::string name(buffer);
-
     {
         std::lock_guard<std::mutex> lock(game_mutex);
-        players[name] = {client_socket, starting_budget};
+        players[name] = {client_socket, starting_budget, 0, false};
         turn_order.push_back(name);
     }
-    
-    std::cout << "[JOIN] " << name << " connected." << std::endl;
     broadcast_game_state();
 
     while (true) {
         memset(buffer, 0, 1024);
         valread = recv(client_socket, buffer, 1024, 0);
         if (valread <= 0) break;
-        std::string data(buffer);
 
-        bool state_changed = false;
+        std::string action(buffer);
+        action.erase(std::remove_if(action.begin(), action.end(), [](char c) { return !isprint(c); }), action.end());
+
+        bool changed = false;
         {
             std::lock_guard<std::mutex> lock(game_mutex);
-            if (name == turn_order[turn_index] && isdigit(data[0])) {
-                int bet = std::stoi(data);
-                if (players[name].wallet >= bet) {
-                    players[name].wallet -= bet;
-                    total_pot += bet;
-                    turn_index = (turn_index + 1) % turn_order.size();
-                    state_changed = true;
-                    std::cout << "[BET] " << name << " bet $" << bet << std::endl;
+            if (name == turn_order[turn_index]) {
+                Player &p = players[name];
+
+                if (action == "FOLD") {
+                    p.folded = true;
+                    next_turn();
+                    changed = true;
+                } else if (action == "CHECK") {
+                    // CALL Logic: Pay the difference to match last_bet
+                    int diff = last_bet - p.round_contribution;
+                    if (p.wallet >= diff) {
+                        p.wallet -= diff;
+                        p.round_contribution += diff;
+                        total_pot += diff;
+                        next_turn();
+                        changed = true;
+                    }
+                } else if (action.find("BET:") == 0) {
+                    int amount = std::stoi(action.substr(4));
+                    // RAISE Logic: Total contribution must be >= last_bet
+                    if (p.wallet >= amount && (p.round_contribution + amount) >= last_bet) {
+                        p.wallet -= amount;
+                        p.round_contribution += amount;
+                        total_pot += amount;
+                        last_bet = p.round_contribution; // New High Bet
+                        next_turn();
+                        changed = true;
+                    }
                 }
             }
         }
-        
-        if (state_changed) {
-            broadcast_game_state();
-        }
+        if (changed) broadcast_game_state();
     }
-    
-    // Cleanup on disconnect
-    std::cout << "[LEAVE] " << name << " disconnected." << std::endl;
     close(client_socket);
 }
 
 int main() {
     std::cout << "Starting Budget: "; std::cin >> starting_budget;
-
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     int opt = 1;
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
     struct sockaddr_in address;
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(65432);
-
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Bind failed");
-        return -1;
-    }
-
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
     listen(server_fd, 5);
-    std::cout << "BANKER LIVE on port 65432" << std::endl;
-
+    std::cout << "BANKER ACTIVE." << std::endl;
     while (true) {
         int new_socket = accept(server_fd, NULL, NULL);
         std::thread(handle_player, new_socket).detach();
--- player.cpp
+++ player.cpp
@@ -5,37 +5,51 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <string.h> // Added for memset
+#include <string.h>
 
 int sock = 0;
-std::string myName;
-std::string potStr = "0", walletStr = "0", currentTurnName = "";
+std::string myName, potStr = "0", walletStr = "0", currentTurnName = "", lastBetStr = "0", myContribStr = "0";
 bool isMyTurn = false;
+char raiseInput[10] = "50";
+int letterCount = 2;
+bool inputActive = false;
 
 void listenToServer() {
     char buffer[1024];
     while (true) {
-        memset(buffer, 0, 1024); // Clear buffer every time
+        memset(buffer, 0, 1024);
         int valread = recv(sock, buffer, 1024, 0);
         if (valread > 0) {
             std::string msg(buffer);
-            size_t pPos = msg.find("POT:"), wPos = msg.find("|WALLET:"), tPos = msg.find("|TURN:");
-            if (pPos != std::string::npos && wPos != std::string::npos && tPos != std::string::npos) {
-                potStr = msg.substr(pPos + 4, wPos - (pPos + 4));
-                walletStr = msg.substr(wPos + 8, tPos - (wPos + 8));
-                currentTurnName = msg.substr(tPos + 6);
+            size_t pPos = msg.find("P:"), wPos = msg.find("|W:"), tPos = msg.find("|T:"), lPos = msg.find("|L:"), cPos = msg.find("|C:");
+            if (pPos != std::string::npos) {
+                potStr = msg.substr(pPos + 2, wPos - (pPos + 2));
+                walletStr = msg.substr(wPos + 3, tPos - (wPos + 3));
+                currentTurnName = msg.substr(tPos + 3, lPos - (tPos + 3));
+                lastBetStr = msg.substr(lPos + 3, cPos - (lPos + 3));
+                myContribStr = msg.substr(cPos + 3);
                 isMyTurn = (currentTurnName == myName);
             }
-        } else {
-            break; 
         }
     }
 }
 
+bool DrawButton(Rectangle rect, const char* text, Color color, bool active) {
+    if (!active) {
+        DrawRectangleRec(rect, GRAY);
+        DrawText(text, rect.x + (rect.width/2 - MeasureText(text, 16)/2), rect.y + 15, 16, DARKGRAY);
+        return false;
+    }
+    Vector2 mouse = GetMousePosition();
+    bool hovering = CheckCollisionPointRec(mouse, rect);
+    DrawRectangleRec(rect, hovering ? Fade(color, 0.7f) : color);
+    DrawText(text, rect.x + (rect.width/2 - MeasureText(text, 16)/2), rect.y + 15, 16, WHITE);
+    return (hovering && IsMouseButtonPressed(MOUSE_LEFT_BUTTON));
+}
+
 int main() {
-    std::string ip;
-    std::cout << "Enter Your Name: "; std::cin >> myName;
-    std::cout << "Enter Banker IP: "; std::cin >> ip;
+    std::cout << "Name: "; std::cin >> myName;
+    std::cout << "Banker IP: "; std::string ip; std::cin >> ip;
 
     struct sockaddr_in serv_addr;
     sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -43,38 +57,70 @@ int main() {
     serv_addr.sin_port = htons(65432);
     inet_pton(AF_INET, ip.c_str(), &serv_addr.sin_addr);
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        std::cout << "Connection Failed!" << std::endl;
-        return -1;
-    }
-
-    // FIXED: Now sending 'myName' (the one with data) instead of 'name'
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) return -1;
+    inet_pton(AF_INET, ip.c_str(), &serv_addr.sin_addr);
+    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
     send(sock, myName.c_str(), myName.length(), 0);
-    
     std::thread(listenToServer).detach();
 
-    InitWindow(400, 450, "Poker - Player");
+    InitWindow(500, 650, "Poker Pro");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
-        if (isMyTurn && IsKeyPressed(KEY_SPACE)) {
-            send(sock, "10", 2, 0);
+        if (inputActive) {
+            int key = GetCharPressed();
+            while (key > 0) {
+                if ((key >= '0' && key <= '9') && (letterCount < 9)) {
+                    raiseInput[letterCount] = (char)key;
+                    raiseInput[letterCount+1] = '\0';
+                    letterCount++;
+                }
+                key = GetCharPressed();
+            }
+            if (IsKeyPressed(KEY_BACKSPACE) && letterCount > 0) {
+                letterCount--;
+                raiseInput[letterCount] = '\0';
+            }
         }
 
         BeginDrawing();
             ClearBackground(GetColor(0x1a1a1aff));
             
-            DrawText(TextFormat("POT: $%s", potStr.c_str()), 120, 40, 30, GOLD);
-            DrawText(TextFormat("WALLET: $%s", walletStr.c_str()), 40, 100, 20, GREEN);
-
-            DrawRectangle(20, 160, 360, 80, isMyTurn ? DARKGREEN : DARKGRAY);
-            std::string turnText = isMyTurn ? "YOUR TURN!" : "WAITING FOR: " + currentTurnName;
-            DrawText(turnText.c_str(), 40, 190, 20, WHITE);
-
-            if(isMyTurn) DrawText("Press [SPACE] to bet $10", 80, 380, 18, YELLOW);
-            else DrawText("Wait for your turn...", 110, 380, 18, GRAY);
+            int callAmount = std::stoi(lastBetStr) - std::stoi(myContribStr);
             
-            DrawText(TextFormat("Playing as: %s", myName.c_str()), 10, 430, 10, GRAY);
+            DrawText(TextFormat("POT: $%s", potStr.c_str()), 180, 40, 30, GOLD);
+            DrawText(TextFormat("WALLET: $%s", walletStr.c_str()), 40, 100, 20, GREEN);
+            DrawText(TextFormat("Total In Round: $%s", myContribStr.c_str()), 40, 130, 20, SKYBLUE);
+            DrawText(TextFormat("To Call: $%d", callAmount), 40, 160, 20, MAROON);
+
+            DrawRectangle(20, 200, 460, 60, isMyTurn ? DARKGREEN : DARKGRAY);
+            DrawText(isMyTurn ? "YOUR TURN" : TextFormat("WAITING FOR: %s", currentTurnName.c_str()), 40, 220, 20, WHITE);
+
+            if (isMyTurn) {
+                if (DrawButton({50, 300, 180, 50}, "FOLD", RED, true)) send(sock, "FOLD", 4, 0);
+                
+                std::string callText = (callAmount == 0) ? "CHECK" : "CALL $" + std::to_string(callAmount);
+                if (DrawButton({270, 300, 180, 50}, callText.c_str(), BLUE, true)) send(sock, "CHECK", 5, 0);
+                
+                if (DrawButton({270, 410, 180, 50}, "RAISE", DARKGREEN, true)) {
+                    std::string action = "BET:" + std::string(raiseInput);
+                    send(sock, action.c_str(), action.length(), 0);
+                }
+            }
+            
+            DrawText("CUSTOM RAISE:", 50, 380, 18, LIGHTGRAY);
+            Rectangle textBox = {50, 410, 180, 50};
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) inputActive = CheckCollisionPointRec(GetMousePosition(), textBox);
+            DrawRectangleRec(textBox, inputActive ? WHITE : GRAY);
+            DrawText(raiseInput, textBox.x + 10, textBox.y + 15, 20, BLACK);
+
+            if (DrawButton({270, 410, 180, 50}, "CONFIRM RAISE", DARKGREEN, isMyTurn)) {
+                std::string action = "BET:" + std::string(raiseInput);
+                send(sock, action.c_str(), action.length(), 0);
+            }
+
+            DrawRectangle(0, 550, 500, 50, BLACK);
+            DrawText(TextFormat("Local: [%s] vs Server: [%s]", myName.c_str(), currentTurnName.c_str()), 10, 565, 12, GRAY);
         EndDrawing();
     }
     CloseWindow();
